9a226246d42735cc9cd5010372e7a66acfd522bd
Ray <cilver2965@naver.com>
2025-11-05 17:12:48 +0900

    Update myart4.py
    This program uses the Turtle graphics library to create a 10x10 grid of colorful dots, similar to a Hirst painting style.
    Each dot is randomly colored using RGB values, and after every 10 dots, the turtle moves up one row and starts a new line.

--- myart4.py
+++ myart4.py
@@ -1,25 +1,29 @@
 import turtle as t
 import random
 
-t.bgcolor("cornsilk")
-t.colormode(255)
-t.speed(0)
-t.penup()
-t.hideturtle()
+# set up the drawing environment
+t.bgcolor("cornsilk") # Set background color
+t.colormode(255)      # Use RGB color mode
+t.speed(0)            # Set turtle speed to the fastest
+t.penup()             # Lift the pen to avoid drawing lines
+t.hideturtle()        # Hide the turtle icorn 
 
+# Move the turtle to the starting position
 t.setheading(225)
 t.forward(300)
 t.setheading(0)
 
-num_dots = 100
+# Total number of dots to draw
+num_dots = 100 
 
+# Draw a 10 X 10 grid of colorful dots
 for i in range(1,num_dots + 1):
     r = random.randint(50,255)
     g = random.randint(50,255)
     b = random.randint(50,255)
     t.dot(20, (r, g, b))
     t.forward(50)
-
+# After every 10 dots, move up to the next row
     if i % 10 == 0:
         t.setheading(90)
         t.forward(50)
