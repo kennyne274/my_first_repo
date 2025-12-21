96aac632bc15a8fd2ce6ab76c18af3757156a9f8
Ray <cilver2965@naver.com>
2025-12-21 23:35:42 +0900

    Create rose.py

---
+++ rose.py
@@ -0,0 +1,15 @@
+import turtle as t
+
+t.bgcolor("black")
+t.speed(0)
+
+colors = ["pink", "hotpink"]
+
+for i in range(200):
+    t.color(colors[i%2])
+    t.pensize(i /50)
+    t.forward(i)
+    t.left(65)
+
+t.hideturtle()
+t.done()
