9b3d11a7a81943ebec863d20043cc518222065c1
Ray <cilver2965@naver.com>
2025-12-25 23:37:11 +0900

    Update calculator01.py

--- calculator/calculator01.py
+++ calculator/calculator01.py
@@ -18,7 +18,7 @@ def calculate(op):
             result = num1*num2  
         elif op == "/":
             if num2 == 0:
-                label.config(text="0으로 나눌 수 없습니다.")
+                label.config(text="Cannot divide by zero.")
                 return 
             result = num1/num2 
            
@@ -31,7 +31,7 @@ def calculate(op):
 
         
 root = tk.Tk()
-root.title("미니 계산기")
+root.title("calculator")
 root.geometry("380x480")
 root.configure(bg="lightblue")
 root.resizable(False, False)
@@ -39,10 +39,10 @@ root.resizable(False, False)
 top_space = tk.Frame(root, height=30, bg="lightblue")
 top_space.pack()
 
-tk.Label(root, text="첫번째 숫자 입력", font=FONT,bg="lightblue").pack()
+tk.Label(root, text="first number", font=FONT,bg="lightblue").pack()
 entry1 = tk.Entry(root, font=("Courier", 15,"bold"), width=20)
 entry1.pack(pady=5)
-tk.Label(root, text="두번째 숫자 입력", font=FONT,bg="lightblue").pack()
+tk.Label(root, text="second number", font=FONT,bg="lightblue").pack()
 entry2 = tk.Entry(root, font=("Courier", 15,"bold"), width=20)
 entry2.pack(pady=5)
 
@@ -51,7 +51,7 @@ button_frame.pack(pady=(20,10))
 
 tk.Button(button_frame,text="+", width=5,command=lambda: calculate("+"),font=FONT,bg="lightpink").grid(row=0, column=0, padx=5)
 tk.Button(button_frame, text="-", width=5,command=lambda:calculate("-"),font=FONT,bg="lightpink").grid(row=0, column=1, padx=5)
-tk.Button(button_frame,text="*", width=5,command=lambda:calculate("*"),font=FONT,bg="lightpink").grid(row=0, column=2, padx=5)
+tk.Button(button_frame,text="x", width=5,command=lambda:calculate("*"),font=FONT,bg="lightpink").grid(row=0, column=2, padx=5)
 tk.Button(button_frame,text="÷", width=5,command=lambda:calculate("/"),font=FONT,bg="lightpink").grid(row=0, column=3, padx=5)
 label = tk.Label(root,text="",font=("Courier", 15, "bold"),bg="ivory", fg="navy",width=20)
 label.pack()
