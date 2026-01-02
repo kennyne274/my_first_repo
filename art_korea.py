a2ffb00051a7b17ac1c4e4101cd0ce976517a2a6
Ray <cilver2965@naver.com>
2026-01-02 18:21:45 +0900

    Create art_korea.py

---
+++ art_korea.py
@@ -0,0 +1,18 @@
+
+import turtle as t
+
+colors =["red","yellow","blue"]
+t.bgcolor("black")
+t.speed(0)
+t.hideturtle()
+i = 0
+while True:
+    t.color(colors[i%3])
+    t.forward(i)
+    t.lt(118)
+    i += 1
+    if i ==900:
+        break
+
+
+t.done()
