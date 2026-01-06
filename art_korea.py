34ba13b48afc05a9475112b4f0a68bc32d12201e
Ray <cilver2965@naver.com>
2026-01-06 23:15:27 +0900

    Refactor turtle art code into draw_art function
    Refactor turtle drawing code into a function for better structure and reusability.

--- art_korea.py
+++ art_korea.py
@@ -1,18 +1,23 @@
-
 import turtle as t
 
-colors =["red","yellow","blue"]
-t.bgcolor("black")
-t.speed(0)
-t.hideturtle()
-i = 0
-while True:
-    t.color(colors[i%3])
-    t.forward(i)
-    t.lt(118)
-    i += 1
-    if i == 1000:
-        break
 
+def draw_art(turn, steps):
+
+    colors =["red","yellow","blue"]
+    t.bgcolor("black")
+    t.speed(0)
+    t.pensize(1)
+    t.hideturtle()
+    i = 0
+    while True:
+        t.color((colors[i%len(colors)]))
+        t.forward(i)
+        t.lt(turn)
+        i += 1
+        if i ==steps:
+            break
+
+if __name__=="__main__":
+    draw_art(118, 1000)
 
 t.done()
