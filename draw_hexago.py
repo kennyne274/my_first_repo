21ef517edd6d23a5599a0a2650a6d23fc230691f
Ray <cilver2965@naver.com>
2026-01-16 23:21:08 +0900

    Update draw_hexago.py

--- draw_hexago.py
+++ draw_hexago.py
@@ -1,35 +1,40 @@
+
+
+#This Python script uses the turtle module 
+#to create a visually striking animation of 100 colorful hexagons rotating on a black background.
+
 import turtle
 import random
 
-# 터틀 설정
+# Basic setup
 t = turtle.Turtle()
 turtle.bgcolor("black")
 t.speed(0)
 t.pensize(1)
 
-# RGB 모드로 전환 (0~255 값 사용 가능)
+# RGB 
 turtle.colormode(255)
 
-# 육각형을 그리는 함수
+# drawing hexagon
 def draw_hexagon(size):
     for _ in range(6):
         t.forward(size)
         t.right(60)
 
-# 회전하며 무작위 색으로 육각형을 그리기
+
 for i in range(100):
-    # 무작위 RGB 색상 생성
+  
     r = random.randint(50, 255)
     g = random.randint(50, 255)
     b = random.randint(50, 255)
     t.color(r, g, b)
     
-    # 육각형 그리기
+   
     draw_hexagon(100)
     
-    # 회전 각도
+   
     t.right(3.6)
 
-# 터틀 숨기기
+
 t.hideturtle()
 turtle.done()
