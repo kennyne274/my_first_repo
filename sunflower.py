9358495db41a0b35a4d9dd52a9b4a15c488c82b7
Ray <cilver2965@naver.com>
2026-02-01 01:46:00 +0900

    Update sunflower.py

--- sunflower.py
+++ sunflower.py
@@ -6,15 +6,19 @@ screen.setup(420, 340)
 screen.bgcolor("black")
 
 t.speed(0)
-colors =["orange", "white"]
-t.width(1)
 
-angle = 137.5
+def sunflower(a, b):
+    colors =[a, b]
+    t.width(1)
 
-for i in range(400):
-    t.pencolor(colors[i%2])
-    t.forward(i * 0.6)
-    t.right(angle)
+    angle = 137.5
+
+    for i in range(300):
+        t.pencolor(colors[i%2])
+        t.forward(i * 0.6)
+        t.right(angle)
+   
+sunflower("orange", "white")
 
 t.hideturtle()
 screen.mainloop()
