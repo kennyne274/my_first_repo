eabc3847cc308eba59c9b3e8248828ebaeb2287f
Ray <cilver2965@naver.com>
2026-02-01 01:41:20 +0900

    Update star.py

--- star.py
+++ star.py
@@ -6,10 +6,10 @@ t.speed(0)
 colors = ["red", "yellow", "blue", "teal", "cyan", "magenta"]
 
 for i in range(200):
-    t.color(colors[i % len(colors)])   # 색상 반복
-    t.forward(i * 2)                   # 점점 길어지는 선
-    t.right(144)                   # 별 모양 각도 (360 / 5 * 2)
-    t.pensize(2)                       # 선 두께
+    t.color(colors[i % len(colors)])   
+    t.forward(i * 2)                  
+    t.right(144)                  
+    t.pensize(2)                       
 
 
 
