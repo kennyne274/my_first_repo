6eaefcbac0349f82fd5fd6f346d9a227c4a612e5
Ray <cilver2965@naver.com>
2026-02-08 00:13:14 +0900

    fantastic animation

---
+++ Fantasy.py
@@ -0,0 +1,22 @@
+import turtle
+import colorsys
+
+t = turtle.Turtle()
+
+turtle.bgcolor("black")
+t.speed(0)
+t.hideturtle()
+turtle.tracer(2)
+
+h = 0
+
+for i in range(1200):
+    c = colorsys.hsv_to_rgb(h,1,0.9)
+    h+= 0.003
+    t.color(c)
+    t.left(149)
+    for j in range(5):
+        t.forward(200)
+        t.left(144)
+turtle.exitonclick()
+
