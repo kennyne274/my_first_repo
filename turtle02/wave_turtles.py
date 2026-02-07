46a25b4eca7b70943fad6e434a47f48bf2cca574
Ray <cilver2965@naver.com>
2026-02-08 00:42:57 +0900

    Add wave_turtles.py for turtle graphics animation

---
+++ turtle02/wave_turtles.py
@@ -0,0 +1,24 @@
+import turtle
+import colorsys
+
+t = turtle.Turtle()
+
+turtle.bgcolor("black")
+t.speed(0)
+t.shape("turtle")
+t.hideturtle()
+
+h = 0
+
+t.penup()
+for i in range(200):
+    c = colorsys.hsv_to_rgb(h,1,0.9)
+    h+= 0.007
+    t.color(c)
+    t.stamp()
+    t.forward(i+12)
+    t.left(25)
+
+
+turtle.exitonclick()
+
