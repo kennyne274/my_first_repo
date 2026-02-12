83e90e8847c7a820a888d3d2fb3c3bdecd44ea37
Ray <cilver2965@naver.com>
2026-02-12 18:55:27 +0900

    Update and rename qrcode.py to qrcode_02.py

---
+++ qrcode/qrcode_02.py
@@ -0,0 +1,33 @@
+import qrcode
+from PIL import Image
+
+
+url = 'https://www.google.com'
+QRcode = qrcode.QRCode(error_correction = qrcode.constants.ERROR_CORRECT_H)
+QRcode.add_data(url)
+QRcode.make()
+
+QRcolor = "#E504E5"
+QRimg = QRcode.make_image(fill_color = QRcolor, back_color = 'white').convert('RGB')
+
+# Path to your logo image
+logoUrl = r"C:\Users\user\Pictures\apple.png" 
+# Open the logo image using PIL
+logo = Image.open(logoUrl)
+
+# Resize the logo to a reasonable size
+base_width = 80
+width_percent = base_width / logo.size[0]
+new_height = int(logo.size[1] * width_percent)
+
+logo = logo.resize((base_width, new_height), Image.Resampling.LANCZOS)
+
+# Calculate center position so the logo sits in the middle of the QR
+center_x = (QRimg.size[0] - logo.size[0]) // 2
+center_y = (QRimg.size[1] - logo.size[1]) // 2
+
+# Paste the logo onto the QR code imag
+QRimg.paste(logo, (center_x, center_y))
+
+QRimg.save('test3.png')
+QRimg
