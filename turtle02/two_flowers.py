bff533e93d09abde66a1282a0152849ed57258ce
Ray <cilver2965@naver.com>
2026-02-12 20:08:24 +0900

    Update two_flowers.py

--- turtle02/two_flowers.py
+++ turtle02/two_flowers.py
@@ -1,7 +1,7 @@
 import turtle as t
 import colorsys
 t.bgcolor("black")
-t.tracer(10)
+t.tracer(20)
 t.pensize(1)
 t.hideturtle()
 
@@ -11,18 +11,17 @@ def flower(x,y, num):
     t.penup()
     t.goto(x,y)
     t.pendown()
-    for i in range(300):
+    for i in range(400):
         c = colorsys.hsv_to_rgb(h, 1, 1)
         t.pencolor(c)
     
         for j in range(2):
 
-            t.circle(100-i/3,180)
+            t.circle(150-i/3,180)
             t.lt(100)
         h += num
-           
-flower(40,40,0.005)
-flower(-120,0,0.0072)
+
+flower(-160,-120,0.0072)
 
 t.update()
 t.done()
