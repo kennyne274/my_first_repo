265d94cbd4ab1e6b53114cad16e8cb0382462ab9
Ray <cilver2965@naver.com>
2026-02-15 20:27:37 +0900

    Update clouds_and_bluesky.py

--- clouds_and_bluesky.py
+++ clouds_and_bluesky.py
@@ -42,7 +42,7 @@ def draw_sky_gradient(steps):
 def draw_circle(x, y, radius, color):
     """지정 위치(x, y)에 색상과 반지름을 가진 원을 그림"""
     t.penup()
-    t.goto(x, y - radius)  # 원의 중심이 아니라 아래쪽에서 시작
+    t.goto(x, y - radius)  
     t.pendown()
     t.color(color, color)
     t.begin_fill()
