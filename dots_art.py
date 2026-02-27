1e9eb1717e7cae16e766c5329b45f92dec0d06d7
Ray <cilver2965@naver.com>
2026-02-28 03:11:55 +0900

    Update dots_art.py

--- dots_art.py
+++ dots_art.py
@@ -14,20 +14,17 @@ t.pendown()
 
 t.colormode(255)
 
-for i in range(10):
-        
+for i in range(10):    
     for j in range(11):
-        t.begin_fill()
-        r = random.randint(30, 250)
-        g = random.randint(30, 250)
-        b = random.randint(30, 250)
-
+        r = random.randint(50, 250)
+        g = random.randint(50, 250)
+        b = random.randint(50, 250)
         t.color(r,g,b)
         t.dot(30)
-        t.end_fill
         t.penup()
         t.fd(60)
         t.pendown()
+       
 
     y += 60
 
