c8fde9a5fb5d409b7bc87ab95ba1fe032baed102
Ray <cilver2965@naver.com>
2026-02-27 23:52:08 +0900

    Create dots_art.py

---
+++ dots_art.py
@@ -0,0 +1,40 @@
+import turtle as t
+import random
+
+t.bgcolor("ivory")
+t.speed(0)
+
+t.penup()
+
+x = -300
+y = -270
+
+t.goto(x, y)
+t.pendown()
+
+t.colormode(255)
+
+for i in range(10):
+        
+    for j in range(11):
+        t.begin_fill()
+        r = random.randint(30, 250)
+        g = random.randint(30, 250)
+        b = random.randint(30, 250)
+
+        t.color(r,g,b)
+        t.dot(30)
+        t.end_fill
+        t.penup()
+        t.fd(60)
+        t.pendown()
+
+    y += 60
+
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+
+t.hideturtle()
+
+t.done()
