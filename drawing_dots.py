d0d53b2f91708fefac964e20e4cef63b0e0a08d2
Ray <cilver2965@naver.com>
2026-03-01 00:49:29 +0900

    Update drawing_dots.py

--- drawing_dots.py
+++ drawing_dots.py
@@ -1,37 +1,44 @@
-#modern art
 import turtle as t
 import random
 
-# set up the drawing environment
-t.bgcolor("cornsilk") # Set background color
-t.colormode(255)      # Use RGB color mode
-t.speed(0)            # Set turtle speed to the fastest
-t.penup()             # Lift the pen to avoid drawing lines
-t.hideturtle()        # Hide the turtle icorn 
-
-# Move the turtle to the starting position
-t.setheading(225)
-t.forward(300)
-t.setheading(0)
+"""It's another way of drawing dots. 
+I have been studying Python for 5 months. 
+I'm not a good student. I want someone to give me advice"""
 
 # Total number of dots to draw
-num_dots = 100 
+num_dot = 100
 
-# Draw a 10 X 10 grid of colorful dots
-for i in range(1,num_dots + 1):
-    r = random.randint(50,255)
-    g = random.randint(50,255)
-    b = random.randint(50,255)
-    t.dot(20, (r, g, b))
-    t.forward(50)
-# After every 10 dots, move up to the next row
-    if i % 10 == 0:
-        t.setheading(90)
-        t.forward(50)
-        t.setheading(180)
-        t.forward(500)
+def random_color():
+    r = random.randint(50, 250)
+    g = random.randint(50, 250)
+    b = random.randint(50, 250)
+    colors = (r,g,b)
+    return colors
+
+def draw_dots():
+    for i in range(1, num_dot+1):
         t.setheading(0)
+        t.dot(25, random_color())
+        t.fd(65)
+        
+        if i % 10 == 0:
+            t.setheading(90)
+            t.fd(60)
+            t.setheading(180)
+            t.fd(650)
 
+
+t.bgcolor("ivory")
+t.speed(0)
+t.penup()
+
+x = -300
+y = -270
+
+t.goto(x, y)
+t.colormode(255)
+
+draw_dots()
+t.hideturtle()
 
 t.done()
-
