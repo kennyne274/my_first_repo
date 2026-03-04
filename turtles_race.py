50c05761aa9f67eb5981aa0519c537143fe0c295
Ray <cilver2965@naver.com>
2026-03-04 23:01:07 +0900

    Update and rename race_of_turtles.py to turtles_race.py

---
+++ turtles_race.py
@@ -0,0 +1,109 @@
+import turtle as t
+import winsound
+import time
+import random
+
+screen = t.Screen()
+screen.setup(800, 600)
+screen.bgcolor("ivory")
+
+colors = ["hotpink", "white", "red", "green", "yellow", "purple", "blue"]
+start_ycor =[150, 90, 30, -30, -90, -150, -210]
+game_over = False
+turtles =[]
+
+t.up()
+t.ht()
+
+# Countdown before race
+t.goto(0, 0)
+t.color("red")
+for count in ["3", "2", "1", "GO!"]:
+    t.clear()
+    t.write(count, align="center", font=("Arial", 120, "bold"))
+    time.sleep(1)
+
+# title
+t.goto(0, 240)
+t.color("teal")
+t.write("Turtle Race", align= "center", font=("Arial", 30, "bold"))
+t.speed(0)
+
+# Draw race track
+t.goto(-400, 170)
+t.down()
+t.color("#0CC7CE")
+t.begin_fill()
+for i in range(2):
+    t.fd(800)
+    t.right(90)
+    t.fd(400)
+    t.right(90)
+t.end_fill()
+
+# Draw finish line
+t.color("white")
+t.up()
+t.goto(330, 200)
+t.down()
+t.goto(330, -250)
+
+# Draw starting lines for each lane
+for i in range(6):
+    t.up()
+    t.goto(-350, start_ycor[i] - 30)
+    t.color("white")
+    t.down()
+    t.goto(350, start_ycor[i] - 30)
+
+
+# Create 7 turtles
+for i in range(7):
+    new_turtle = t.Turtle()
+    new_turtle.up()
+    new_turtle.shape("turtle")
+    new_turtle.color(colors[i])
+    new_turtle.goto(-350, start_ycor[i])
+    new_turtle.write(i+1) # 터틀 번호는 1부터 설정
+    new_turtle.goto(-330, start_ycor[i])
+    turtles.append(new_turtle)
+
+# Get user's bet
+user_choice = int(t.textinput("터틀 레이스", "몇 번에 배팅할래?(1~7)"))
+
+t.up()
+t.goto(0, -290)
+t.color("teal")
+t.write(f"{user_choice}번 터틀에 배팅하였습니다.", align= "center", font=("Arial", 18, "bold"))
+
+
+# sound race start
+winsound.Beep(523, 300)
+time.sleep(0.3)
+
+# game loop
+while not game_over:
+    for i in turtles:
+        rand_speed = random.randint(1, 10)
+        i.forward(rand_speed)
+        if i.xcor() > 330:
+            game_over = True
+            break
+
+# Find winner
+max_xcor = 0
+winner = 0
+for i in range(len(turtles)):
+    if turtles[i].xcor() > max_xcor:
+        max_xcor = turtles[i].xcor()
+        winner = i + 1
+
+# show result
+t.goto(0, 0)
+t.color("red")
+if user_choice == winner:
+    t.write(f"You've won!!", align= "center", font=("Arial", 20, "bold"))
+else:
+    t.write("실패!! 100$를 잃었습니다.", align= "center", font=("Arial", 20, "bold"))
+
+t.done()
