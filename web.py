4bd13030de36d228f04ec86956ea2f810187317a
Ray <cilver2965@naver.com>
2026-03-05 00:30:07 +0900

    Create web.py

---
+++ web.py
@@ -0,0 +1,23 @@
+import turtle as t
+import colorsys
+
+t.speed(0)
+t.pensize(1)
+t.bgcolor('black')
+
+hue = 0
+
+for i in range(250):
+    col=colorsys.hsv_to_rgb(hue, 1, 1)
+    t.pencolor(col)
+    hue+=0.005
+    t.circle(6-i, 90)
+    t.lt(80)
+    t.circle(6-i, 90)
+    t.rt(180)
+
+t.up()
+t.goto(0,0)
+t.shape("turtle")
+t.shapesize(3,3,3)
+t.done()
