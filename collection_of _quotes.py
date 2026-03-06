d0ac203b51d8dfaa0ce22bb012ba3aeb64ef1797
Ray <cilver2965@naver.com>
2026-03-06 17:55:16 +0900

    Update collection_of _quotes.py

--- collection_of _quotes.py
+++ collection_of _quotes.py
@@ -9,33 +9,44 @@ text = [
         "Don’t cry because it’s over, smile because it happened.",
         "All you need is love.", 
         "To thine own self be true.",
-        "Where there is a will, there is a way.",
+        "No pain, no gain",
         "The best is yet to come.",
         "Honesty is the best policy.",
     ]
 
 
 def random_color():
-    r = random.randint(0, 250)
-    g = random.randint(0, 250)
-    b = random.randint(0, 250)
+    r = random.randint(70, 250)
+    g = random.randint(70, 250)
+    b = random.randint(70, 250)
     colors = (r,g,b)
     return colors
-    
+
 t.shapesize(3)
 t.setheading(90)
 t.speed(2)
-t.bgcolor("cornsilk")
+t.bgcolor("black")
 t.colormode(255)
 t.shape("turtle")
 t.color("gold")
 t.penup()
 t.goto(0, -300)
 
-for i in range(8):
+for i in range(len(text)):
     t.write(text[i], align="center", font=("Times New Roman", 20, "bold"))
     t.color(random_color())
     t.fd(70)
 
-t.done()
+tt = t.Turtle()
+tt.ht()
+tt.speed(0)
 
+for i in range(50):
+    x = random.uniform(-300, 300)
+    y = random.uniform(-300, 300)
+    size = random.uniform(2, 6)
+    tt.up()
+    tt.goto(x, y)
+    tt.dot(size, "white")
+
+t.done()
