4301b7809436b75fd9ff0cbacd159a9d6e6f07f8
Ray <cilver2965@naver.com>
2026-03-29 02:21:33 +0900

    Update Taegeuk.py

--- Taegeuk.py
+++ Taegeuk.py
@@ -2,40 +2,41 @@ import turtle as t
 
 # draw Taegeuk
 
-radius = 120
-
-t.bgcolor("black")
-
 t.setup(600, 400)
-t.up()
-t.goto(radius, 0)
-t.down()
-t.seth(90)
+t.bgcolor("black")
+t.title("Taegeuk")
 
-t.color("#EE2132")              
-t.begin_fill()
-t.circle(radius, 180)          
-t.end_fill()
+def Taegeuk(radius=80):
+    t.up()
+    t.goto(radius, -33) 
+    t.down()
+    t.seth(90)
 
+    t.color("#EE2132")              
+    t.begin_fill()
+    t.circle(radius, 180)          
+    t.end_fill()
 
-t.color("#0C62D2")              
-t.begin_fill()
-t.circle(radius, 180)           
-t.end_fill()
+    t.color("#0C62D2")              
+    t.begin_fill()
+    t.circle(radius, 180)           
+    t.end_fill()
 
-t.seth(90)
-t.color("#0C62D2")             
-t.begin_fill()
-t.circle(radius/2, 180) 
-t.end_fill()
+    t.seth(90)
+    t.color("#0C62D2")             
+    t.begin_fill()
+    t.circle(radius/2, 180) 
+    t.end_fill()
 
-t.seth(180)
-t.fd(radius)
-t.seth(270)
-t.color("#EE2132")              
-t.begin_fill()
-t.circle(radius/2, 180) 
-t.end_fill()
+    t.seth(180)
+    t.fd(radius)
+    t.seth(270)
+    t.color("#EE2132")              
+    t.begin_fill()
+    t.circle(radius/2, 180) 
+    t.end_fill()
+    t.ht()
 
-t.ht()
+Taegeuk()
+
 t.done()
