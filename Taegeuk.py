5b2f65cbe6dbeca32912ad4bb449c173a3d862a5
Ray <cilver2965@naver.com>
2026-03-28 22:14:28 +0900

    Add comment about Taegeuk symbol in Taegeuk.py
    Added a comment explaining the Taegeuk symbol.

---
+++ Taegeuk.py
@@ -0,0 +1,42 @@
+import turtle as t
+
+# draw Taegeuk
+# This symbol is used for the flag of Korea
+
+radius = 120
+
+t.bgcolor("black")
+
+t.setup(600, 400)
+t.up()
+t.goto(radius, 0)
+t.down()
+t.seth(90)
+
+t.color("#EE2132")              
+t.begin_fill()
+t.circle(radius, 180)          
+t.end_fill()
+
+
+t.color("#0C62D2")              
+t.begin_fill()
+t.circle(radius, 180)           
+t.end_fill()
+
+t.seth(90)
+t.color("#0C62D2")             
+t.begin_fill()
+t.circle(radius/2, 180) 
+t.end_fill()
+
+t.seth(180)
+t.fd(radius)
+t.seth(270)
+t.color("#EE2132")              
+t.begin_fill()
+t.circle(radius/2, 180) 
+t.end_fill()
+
+t.ht()
+t.done()
