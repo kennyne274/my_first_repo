4f31be97071c648bde99a0d0c8cab6b817c4c30a
Ray <cilver2965@naver.com>
2026-04-07 21:28:36 +0900

    Add flower drawing functionality with turtle graphics

---
+++ flower2.py
@@ -0,0 +1,78 @@
+import turtle as t
+import time
+import random
+
+t.bgcolor("black")
+t.speed(0)
+t.hideturtle()
+
+def draw_stem(height=35):
+    size = height
+    t.penup()
+    t.goto(0, -330)         
+    t.pendown()
+    t.color("#228B22")      
+    
+    for i in range(0, height): 
+        t.seth(90)  
+        t.fd(10)
+        t.pensize(size)
+        time.sleep(0.02)   
+        size -= 1     
+
+
+def draw_leaf(x, y, angle, height=32):
+    size = height *2
+    t.penup()
+    t.goto(x, y)          # 시작 위치 (화면 아래)
+    t.pendown()
+    t.color("#228B22")      
+    
+    for j in range(0, height): 
+        t.seth(angle)  
+        t.fd(7)
+        t.pensize(size)
+        time.sleep(0.02)   
+        size -= 2     
+        
+def draw_petal():
+    t.color("#FE5972")
+    t.begin_fill()
+    t.circle(160, 60)
+    t.left(120)
+    t.circle(160, 60)
+    t.left(120)
+    t.end_fill()
+
+def draw_flower():
+    t.penup()
+    t.goto(0, 0)
+    t.pendown()
+    for f in range(20):
+        draw_petal()
+        t.left(18)
+
+    # flower center
+    t.penup()
+    t.goto(0, 0)
+    t.dot(120, "yellow")
+    t.end_fill()
+
+    t.goto(0, 0)
+    t.pendown()
+    center()
+
+
+def center():
+    t.color("brown")
+    for c in range(30):
+        t.circle(30)
+        t.right(12)
+
+
+draw_stem()
+draw_leaf(20, -340, 40)
+draw_leaf(-20, -340, 140)
+draw_flower()
+
+t.done()
