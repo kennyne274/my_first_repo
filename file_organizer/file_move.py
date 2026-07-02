623ef3908d1f4948901477810bf4c9d21a31f480
Ray <cilver2965@naver.com>
2026-07-02 23:37:27 +0900

    Update file_move.py

--- file_organizer/file_move.py
+++ file_organizer/file_move.py
@@ -12,23 +12,20 @@ def file_move(parent_path, source_folder, target_folder):
 
     os.makedirs(target_path, exist_ok=True) 
 
-    try:
-    
+    try:    
         for file in os.listdir(source_path):
             file_path = os.path.join(source_path, file)
-            
+           
             if os.path.isdir(file_path):
-                            continue 
+                continue 
             # Move files with the '.py' or '.ipynb' extension to the target folder.
             if file.lower().endswith((".py", ".ipynb")): 
-
                 try: 
                     shutil.move(file_path, target_path)
                 except PermissionError:
                     print(f"Permission denied: {file}")
                 except shutil.Error:
                     print(f"Failed to move file: {file}")
-
 
     except FileNotFoundError:
         print("The specified path could not be found.")
